6150fe723d6252b015e1af7a8335561bc3a54ba9
uditha gunaratne <uditha.gunaratne@kaleris.com>
2024-06-28 13:01:37 +0530

    added unit tests for builder

--- src/builder.rs
+++ src/builder.rs
@@ -150,3 +150,331 @@ impl QuickStreamBuilder {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tokio_postgres::Config;
+    use tokio_util::sync::CancellationToken;
+
+    use super::{support::QueryHolder, QuickStreamBuilder};
+
+#[test]
+    fn test_builder() {
+        let cancellation_token = CancellationToken::new();
+
+        let db_config = Config::new();
+        let queries = QueryHolder::default();
+
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token.clone())
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(db_config.clone())
+            .queries(queries.clone())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .print_connection_configuration();
+
+        let upsert_processor = builder.build_update();
+        
+        assert_eq!(upsert_processor.max_con_count, 2);
+        assert_eq!(upsert_processor.buffer_size, 10);
+        assert_eq!(upsert_processor.single_digits, 1);
+        assert_eq!(upsert_processor.tens, 2);
+        assert_eq!(upsert_processor.hundreds, 1);
+        // Compare the string representation of db_config as Config doesn't implement PartialEq
+        assert_eq!(format!("{:?}", upsert_processor.db_config), format!("{:?}", db_config));
+        // Compare the string representation of queries as it may not implement PartialEq
+        assert_eq!(format!("{:?}", upsert_processor.queries), format!("{:?}", queries));
+        assert_eq!(upsert_processor.max_records_per_cycle_batch, 10);
+        assert_eq!(upsert_processor.introduced_lag_cycles, 2);
+        assert_eq!(upsert_processor.introduced_lag_in_millies, 10);
+        assert_eq!(upsert_processor.connection_creation_threshold, 15.0);
+        assert_eq!(upsert_processor.print_con_config, true);
+        assert_eq!(upsert_processor.cancellation_token.is_cancelled(), cancellation_token.is_cancelled());
+
+        cancellation_token.cancel();
+        assert_eq!(upsert_processor.cancellation_token.is_cancelled(), cancellation_token.is_cancelled());
+    }
+
+    #[test]
+    #[should_panic(expected = "cancellation_token is None")]
+    fn test_missing_cancellation_token() {
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "max_con_count is None")]
+    fn test_missing_max_con_count() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "buffer_size is None")]
+    fn test_missing_buffer_size() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "single_digits is None")]
+    fn test_missing_single_digits() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "tens is None")]
+    fn test_missing_tens() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "hundreds is None")]
+    fn test_missing_hundreds() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "db_config is None")]
+    fn test_missing_db_config() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "queries is None")]
+    fn test_missing_queries() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "max_records_per_cycle_batch is None")]
+    fn test_missing_max_records_per_cycle_batch() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "introduced_lag_cycles is None")]
+    fn test_missing_introduced_lag_cycles() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_in_millies(10)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "introduced_lag_in_millies is None")]
+    fn test_missing_introduced_lag_in_millies() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .connection_creation_threshold(15.0)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+
+    #[test]
+    #[should_panic(expected = "connection_creation_threshold is None")]
+    fn test_missing_connection_creation_threshold() {
+        let cancellation_token = CancellationToken::new();
+        let mut builder = QuickStreamBuilder::default();
+        builder
+            .cancellation_tocken(cancellation_token)
+            .max_connection_count(2)
+            .buffer_size(10)
+            .single_digits(1)
+            .tens(2)
+            .hundreds(1)
+            .db_config(Config::new())
+            .queries(QueryHolder::default())
+            .max_records_per_cycle_batch(10)
+            .introduced_lag_cycles(2)
+            .introduced_lag_in_millies(10)
+            .name("test".to_string())
+            .print_connection_configuration();
+
+        let _ = builder.build_update();
+    }
+}
